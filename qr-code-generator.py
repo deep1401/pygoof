f69814831b30e6171279c470959dc32ebb1354b7
deep1401 <gandhi0869@gmail.com>
2020-02-09 15:45:59 +0530

    QR Generator

--- qr-code-generator.py
+++ qr-code-generator.py
@@ -4,4 +4,7 @@ def create_qr(link):
     url=pyqrcode.create(link)
     url.svg('uca-url.svg',scale=8)
     print(url.terminal(quiet_zone=1))
+    print('QR code stored in local directory! ')
 
+link=input("Enter link to create url: ")
+create_qr(link)
