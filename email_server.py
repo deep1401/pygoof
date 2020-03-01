f739dc6b579ae3fcc4d17d434ff3cc873f792120
deep1401 <gandhi0869@gmail.com>
2020-03-01 20:23:30 +0530

    Email with attachments

--- email_server.py
+++ email_server.py
@@ -1,11 +1,48 @@
-import smtplib
-email=################
-password=#############
-mailto=##################
-msg='Test Email using SMTP'
-mailServer = smtplib.SMTP('smtp.gmail.com' , 587)
-mailServer.starttls()
-mailServer.login(email, password)
-mailServer.sendmail(email, mailto , msg)
-print(" \n Sent!")
-mailServer.quit()
+import smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+subject = "An email with attachment from Python"
+body = "This is an email with attachment sent from Python"
+sender_email = "gandhi0869@gmail.com"
+receiver_email = 'thisisdeepgandhi@outlook.com'
+
+password = input("Type your password and press enter:")
+
+
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email
+
+
+message.attach(MIMEText(body, "plain"))
+
+filename = "P2232175-01.jpg"
+
+
+with open(filename, "rb") as attachment:
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+
+encoders.encode_base64(part)
+
+
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+
+message.attach(part)
+text = message.as_string()
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465,context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
